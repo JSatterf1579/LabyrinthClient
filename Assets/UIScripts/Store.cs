cd1e5c0cfc200cab8fedec2f4034ec86374d1ebb
freshvictory <jsren1@verizon.net>
2016-02-21 13:03:08 -0500

    Beginning of UI work

---
+++ Assets/UIScripts/Store.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Store : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void OnBack() {
+		SceneManager.LoadScene("MainMenu");
+	}
+}
